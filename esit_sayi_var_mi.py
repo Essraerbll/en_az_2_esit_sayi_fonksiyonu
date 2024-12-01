7a70285a708f34b52633a032918d3e4263de9d66
esrae <esraerbill652@gmail.com>
2024-12-01 15:19:49 +0300

    3 farklı yolla yapıldı

--- esit_sayi_var_mi.py
+++ esit_sayi_var_mi.py
@@ -1,6 +1,40 @@
-def list[sayi1, sayi2, sayi3, sayi4, sayi5]:
+#birinci cozum yolu :
+
+""" def list(sayi1, sayi2, sayi3, sayi4, sayi5):
+    sayilar=[sayi1, sayi2, sayi3, sayi4, sayi5]
+    for i in sayilar:
+        if sayilar.count(i) > 1:
+            return "En az iki sayi birbirine esittir."
+    return 
+   
+print(list(1,5,3,2,4))  """
 
 
+#ikinci cozum yolu :
+
+""" def list(sayi1, sayi2, sayi3, sayi4, sayi5):
+    sayilar=[sayi1, sayi2, sayi3, sayi4, sayi5]
+
+    for i in range(5):
+        for j in range(i+1, 5):
+            if sayilar[i]==sayilar[j]:
+                print("En az iki sayi birbirine esittir.")
+    return
+
+list(1,2,3,4,3) """
+
+
+#ucuncu cozum yolu :
+
+""" def list(sayi1, sayi2, sayi3, sayi4, sayi5):
+    sayilar=[sayi1, sayi2, sayi3, sayi4, sayi5]
+
+    if len(sayilar) != len(set(sayilar)):
+        print("En az iki sayi birbirine esittir.")
+    else:
+        return
+
+list(1,2,3,4,3) """
 
 
     
